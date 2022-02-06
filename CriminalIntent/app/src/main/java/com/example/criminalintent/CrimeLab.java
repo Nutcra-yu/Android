e5f9a78f6d71914655db0f49c02d894e0199efe3
Nutcra <2428859434@qq.com>
2022-02-07 01:30:07 +0800

    第八章（但是打开是crime界面 不是list界面）

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeLab.java
@@ -1,12 +1,17 @@
 package com.example.criminalintent;
 
 import android.content.Context;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class CrimeLab
 {
     private static CrimeLab sCrimeLab;
 
-    private CrimeLab(Context context) { }
+    private List<Crime> mCrimes;
 
     public static CrimeLab get(Context context)
     {
@@ -14,6 +19,31 @@ public class CrimeLab
         { sCrimeLab = new CrimeLab(context); }
 
         return sCrimeLab;
+    }
+
+    private CrimeLab(Context context)
+    {
+        mCrimes = new ArrayList<>() ;
+        for(int i=0 ; i<100 ; i++)
+        {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved( i%2 == 0);
+            mCrimes.add(crime);
+        }
+    }
+
+    public List<Crime> getCrimes()
+    { return mCrimes; }
+
+    public Crime getCrime(UUID id)
+    {
+        for(Crime crime : mCrimes)
+        {
+            if (crime.getId().equals(id))
+                return crime;
+        }
+        return null;
     }
 
 }
